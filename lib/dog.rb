1dc552034217534210e20026645388d93e757cd8
Amanda Richardson <amanda162000@gmail.com>
2018-07-12 04:30:37 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,7 @@
 # dog.rb
+class Dog
+  def initlialize(fido, snoopy, lassie)
+  @fido = fido
+  @snoopy = snoopy
+  @lassie = lassie
+end
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,7 @@
 # person.rb
+class Person
+  def initlialize(adele_goldberg, alan_kay)
+  @adele_goldberg = adele_goldberg
+  @alan_kay = alan_kay
+end
+end
